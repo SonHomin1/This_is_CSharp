23d8bd6877a98e218d66b906c160c23f47a91935
손호민 <sonhomin1130@naver.com>
2023-01-10 21:37:58 +0900

    2023_01_10
    2023_01_10

---
+++ 2023_01_10/source/ch03/Enum2/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enum2
+{
+    class MainApp
+    {
+        enum DialogResult { YES, NO, CENCEL, CONFIRM, OK }
+        static void Main(string[] args)
+        {
+            DialogResult result = DialogResult.YES;
+
+            Console.WriteLine(result == DialogResult.YES);
+            Console.WriteLine(result == DialogResult.NO);
+            Console.WriteLine(result == DialogResult.CENCEL);
+            Console.WriteLine(result == DialogResult.CONFIRM);
+            Console.WriteLine(result == DialogResult.OK);
+        }
+    }
+}
---
+++ 2023_01_10/source/ch03/Enum3/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enum2
+{
+    class MainApp
+    {
+        enum DialogResult { YES = 10, NO, CENCEL, CONFIRM = 50, OK }
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CENCEL);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+        }
+    }
+}
+
